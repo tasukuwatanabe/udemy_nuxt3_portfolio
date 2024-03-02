ab5c3e7572b797586b4ddcce7508c2234815c918
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-03-02 10:47:05 +0900

    Remove Unused Codes From About

--- pages/about.vue
+++ pages/about.vue
@@ -2,13 +2,4 @@
   <div>
     This page is about me.
   </div>
-  <div>
-    <button @click="enableCustomLayout">Update Layout</button>
-  </div>
 </template>
-
-<script setup>
-function enableCustomLayout() {
-  setPageLayout('another');
-}
-</script>
