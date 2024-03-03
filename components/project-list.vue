0c501b67d14df3ced27677d77f6a74c564f4e93b
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-03-03 12:35:49 +0900

    Data Fetching

---
+++ components/project-list.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>projects</div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Something went wrong...</section>
+  <section v-else>Here we display the repos!</section>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch('https://api.github.com/users/piotr-jura-udemy/repos');
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
       </div>
       <Menu />
     </header>
-    <main class="p-2">
+    <main class="p-2 mt-10">
       <slot />
     </main>
   </div>
