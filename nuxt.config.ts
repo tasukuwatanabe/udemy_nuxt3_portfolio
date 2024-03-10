0f707e650379229652170fa5de7a72542c598176
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-03-10 23:13:58 +0900

    Images and Code Highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,14 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/content'
   ],
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
   colorMode: {
     classSuffix: ''
   }
